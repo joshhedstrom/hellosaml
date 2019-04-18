dc40ec46d6c051cb41be273a4c791aa0a0f7ff4d
Josh Hedstrom <josh.hedstrom@gmail.com>
2019-04-18 14:17:58 -0600

    init

---
+++ src/main/java/com/example/hellosaml/HellosamlApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellosaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellosamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellosamlApplication.class, args);
+	}
+
+}
