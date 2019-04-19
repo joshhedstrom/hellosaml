c0425ff7a634afdce1cb17d09a7aea9858707f33
Josh Hedstrom <josh.hedstrom@gmail.com>
2019-04-19 15:17:09 -0600

    imported addArgumentResolvers

--- src/main/java/com/hedstrom/hellosaml/config/MvcConfig.java
+++ src/main/java/com/hedstrom/hellosaml/config/MvcConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
