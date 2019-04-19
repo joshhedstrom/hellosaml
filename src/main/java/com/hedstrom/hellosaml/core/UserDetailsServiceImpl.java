5151f39e68e914c27d2e79b6085fc88071a6bdff
Josh Hedstrom <josh.hedstrom@gmail.com>
2019-04-19 15:32:50 -0600

    scaffolded

---
+++ src/main/java/com/hedstrom/hellosaml/core/UserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package com.hedstrom.hellosaml.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.saml.SAMLCredential;
+import org.springframework.security.saml.userdetails.SAMLUserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements SAMLUserDetailsService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+
+    public Object loadUserBySAML(SAMLCredential credential)
+    throws UsernameNotFoundException {
+
+        String userID = credential.getNameID().getValue();
+
+        LOG.info(userID + " was logged in");
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER");
+        authorities.add(authority);
+
+        
+        
+        return new User(userID, "<testPassword>", true, true, true, true, authorities);
+    }
+
+
+}
